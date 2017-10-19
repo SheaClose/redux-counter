91a92d3bf9aa0bc045aa9bf74408a27fbab80209
SheaClose <SheaClose@gmail.com>
2017-10-18 19:55:29 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,63 +1,67 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import "./App.css";
+import { decrement, increment, redo, undo } from './ducks/counter';
 
-export class App extends Component {
-	render() {
-		return (
-			<div className="app">
-				<section className="counter">
-					<h1 className="counter__current-value">{ 0 }</h1>
-					<div className="counter__button-wrapper">
-						<button
-							className="counter__button increment-one"
-							onClick={ () => null }
-						>
-							+1
-						</button>
-						<button
-							className="counter__button increment-five"
-							onClick={ () => null }
-						>
-							+5
-						</button>
-						<button
-							className="counter__button decrement-one"
-							onClick={ () => null }
-						>
-							-1
-						</button>
-						<button
-							className="counter__button decrement-five"
-							onClick={ () => null }
-						>
-							-5
-						</button>
-						<br />
-						<button
-							className="counter__button undo"
-							disabled={ true }
-							onClick={ () => null }
-						>
-							Undo
-						</button>
-						<button
-							className="counter__button redo"
-							disabled={ true }
-							onClick={ () => null }
-						>
-							Redo
-						</button>
-					</div>
-				</section>
-				<section className="state">
-					<pre>
-						{ JSON.stringify( this.props, null, 2 ) }
-					</pre>
-				</section>
-			</div>
-		);
-	}
+import './App.css';
+
+class App extends Component {
+  render() {
+    const {
+      currentValue,
+      decrement,
+      futureValues,
+      increment,
+      previousValues,
+      redo,
+      undo
+    } = this.props;
+    return (
+      <div className="app">
+        <section className="counter">
+          <h1 className="counter__current-value">{currentValue}</h1>
+          <div className="counter__button-wrapper">
+            <button className="counter__button" onClick={() => increment(1)}>
+              +1
+            </button>
+            <button className="counter__button" onClick={() => increment(5)}>
+              +5
+            </button>
+            <button className="counter__button" onClick={() => decrement(1)}>
+              -1
+            </button>
+            <button className="counter__button" onClick={() => decrement(5)}>
+              -5
+            </button>
+            <br />
+            <button
+              className="counter__button"
+              disabled={previousValues.length === 0}
+              onClick={undo}
+            >
+              Undo
+            </button>
+            <button
+              className="counter__button"
+              disabled={futureValues.length === 0}
+              onClick={redo}
+            >
+              Redo
+            </button>
+          </div>
+        </section>
+        <section className="state">
+          <pre>{JSON.stringify(this.props, null, 2)}</pre>
+        </section>
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return state;
+}
+
+export default connect(mapStateToProps, { decrement, increment, redo, undo })(
+  App
+);
